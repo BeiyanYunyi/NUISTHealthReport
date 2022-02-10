945f4d187ae730b1a93c51b9b81ae16a87e645b8
lixiang810 <14120445+lixiang810@users.noreply.github.com>
2022-02-10 15:24:26 +0800

    🐛 getMonth 不行

--- src/utils/getWID.ts
+++ src/utils/getWID.ts
@@ -1,6 +1,6 @@
 import timeUtils from './timeUtils';
 
 const getWID = (userID: string, date: Date) =>
-  `${timeUtils.prefix(date.getDate())}-${date.getMonth()}月 -22_${userID}`;
+  `${timeUtils.prefix(date.getDate())}-${date.getMonth() + 1}月 -22_${userID}`;
 
 export default getWID;
